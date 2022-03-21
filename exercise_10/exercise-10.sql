15e050253b679b42e1a983aa1c6e1795044a57af
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-21 14:58:24 -0700

    finished

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,25 @@
 -- find the category name of all inventory items that have never been rented
+SELECT
+  name
+FROM
+  inventory
+LEFT JOIN
+  rental
+ON
+  rental.inventory_id = inventory.inventory_id
+LEFT JOIN
+  film
+ON
+  film.film_id = inventory.film_id
+LEFT JOIN
+  film_category
+ON
+  film_category.film_id = film.film_id
+LEFT JOIN
+  category
+ON
+  category.category_id = film_category.category_id
+WHERE
+  rental.rental_id IS NULL;
+
 -- Similar to exercise 9 with an additonal join
